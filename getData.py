5b0365bf70a6047de1d790cca637f8124d111ed0
Jose Perez Huanca <perezhuancajose@gmail.com>
2024-01-08 21:43:49 -0300

    progressbar

--- getData.py
+++ getData.py
@@ -1,5 +1,7 @@
 import requests;
 import json;
+import wx;
+
 url = 'https://pokeapi.co/api/v2/pokemon/';
 pokemonList=[];
 
@@ -7,15 +9,20 @@ pokemonList=[];
 def updatePokemonData():
     response = requests.get(url);
     data = response.json();
+    total=data['count'];
+    progress=wx.ProgressDialog("Actualizando datos", "Obteniendo datos de https://pokeapi.co...", maximum=total, style=wx.PD_AUTO_HIDE | wx.PD_APP_MODAL);
 
     while data['next']:
         for pokemon in data['results']:
             pokemonList.append(pokemon);
+        (keep_going, skip) = progress.Update(len(pokemonList),newmsg=f"Procesando {len(pokemonList)} de {total} elementos");
+        
         response = requests.get(data['next']);
         data = response.json();
 
     for pokemon in data['results']:
         pokemonList.append(pokemon);
+    (keep_going, skip) = progress.Update(len(pokemonList),newmsg=f"Procesando {len(pokemonList)} de {total} elementos");
 
     with open('pokemon.json','w') as pokemonJson:
         json.dump(pokemonList, pokemonJson, indent=2);
